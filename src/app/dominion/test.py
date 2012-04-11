de01086c0c3a649680f421f9b4350c3253cad92d
Luis Ibáñez <ender@enderscode.com>
2012-04-11 21:58:52 +0200

    fixed a bug

--- src/app/dominion/test.py
+++ src/app/dominion/test.py
@@ -4,24 +4,18 @@ Created on 11/04/2012
 @author: ender3
 '''
 from app.dominion.dominion import Dominion
-from bottle import route, run
 import sys
 
 class Usage(Exception):
     def __init__(self, msg):
         self.msg = msg
 
-    def main():
-        dominion=Dominion()
-        dominion.newPlayer("ender")
-        dominion.newPlayer("pepe")
-        dominion.initGame()
+def main():
+    dominion=Dominion()
+    dominion.newPlayer("ender")
+    dominion.newPlayer("pepe")
+    dominion.initGame()
     
-    @route('/todo')
-    def todo_list():
-        return "hola"
-
-run()
 
 if __name__ == "__main__":
     sys.exit(main())
